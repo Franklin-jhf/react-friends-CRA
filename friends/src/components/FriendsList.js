be4f11cf1274b5b0cafe6b07a023600d352611b0
Franklin Floresca <franklin.jhf@gmail.com>
2017-09-06 14:38:09 -0500

    almost finished

---
+++ friends/src/components/FriendsList.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import Friend from './Friend';
+import frien
+import './App.css';
+
+class FriendsList extends Component {
+  constructor(props) {
+    super(props);
+  
+    this.state = {
+      searchText: '',
+      orderBy: 'name',
+      order: 'ascending'
+    };
+  }
+
+  handleChange( field, event ) {
+    this.setState( {[field]: event.target.value } );
+  }
+
+  render() {
+    return (
+      <div>
+        <form className="form-inline searchForm" role="form">
+          <div className="form-group">
+
+            <input 
+              className = "form-control" 
+              onChange={ this.handle.bind( this, "searchText" ) }
+              placeholder = "Search For Friends"
+              value = { this.state.searchText }
+            />
+
+            <select 
+              className="input-medium"
+              onChange= {this.handleChange.bind( this, "orderBy") }
+              value = { this.state.orderBy }
+            >
+                <option value="name">Name</option>
+                <option value='friend_count'>#Friends</option>
+            </select>
+
+            <select 
+              className="input-medium"
+              onChange= {this.handleChange.bind(this, "order") }
+              value = { this.state.order }
+            >
+                <option value={ 'descending' }>Descending</option>
+                <option value={ 'ascending' }>Ascending</option>
+            </select>
+
+          </div>
+        </form>
+
+        <ul>
+          <Friend />
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default FriendsList;
